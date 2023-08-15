ca6f817fdf48b1706a2c473c4b185a64f5cb161f
Ckm54 <mutaic84@gmail.com>
2023-08-15 14:44:00 +0300

    handle posting message to database

--- src/components/ChatContainer.tsx
+++ src/components/ChatContainer.tsx
@@ -2,6 +2,10 @@ import { IAuthUser, IUser } from "@/types/typings";
 import ChatHeader from "./chat/ChatHeader";
 import ChatsView from "./chat/ChatsView";
 import ChatInputForm from "./chat/ChatInputForm";
+import { toast } from "react-hot-toast";
+import axios from "axios";
+import { BASE_API_URL } from "@/lib/constants";
+import React from "react";
 
 interface ChatContainerProps {
   currentUser: IAuthUser;
@@ -9,6 +13,34 @@ interface ChatContainerProps {
 }
 
 const ChatContainer = ({ currentUser, currentChat }: ChatContainerProps) => {
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [error, setError] = React.useState(false);
+  const [errorMessage, setErrorMessage] = React.useState("");
+  const handleSendMessage = async (message: string) => {
+    setIsLoading(true);
+    try {
+      const requestData = {
+        from: currentUser._id,
+        to: currentChat._id,
+        message,
+      };
+      const response = await axios.post(
+        `${BASE_API_URL}/messages/addmsg`,
+        requestData
+      );
+
+      console.log(response);
+      toast.success("message sent");
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (error: any) {
+      setErrorMessage(error.response.data.message);
+      console.log({ error });
+      setError(true);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
     <div className="flex flex-col h-full">
       {/* chat section header */}
@@ -18,7 +50,12 @@ const ChatContainer = ({ currentUser, currentChat }: ChatContainerProps) => {
         <ChatsView currentUser={currentUser} />
       </div>
       {/* form input */}
-      <ChatInputForm />
+      <ChatInputForm
+        handleSendMessage={handleSendMessage}
+        isLoading={isLoading}
+        error={error}
+        errorMessage={errorMessage}
+      />
     </div>
   );
 };
--- src/components/chat/ChatInputForm.tsx
+++ src/components/chat/ChatInputForm.tsx
@@ -1,5 +1,87 @@
-const ChatInputForm = () => {
-  return <div>ChatInputForm</div>;
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Loader, SendHorizonal } from "lucide-react";
+import React from "react";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { Button } from "../../ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "../../ui/form";
+import { Input } from "../../ui/input";
+
+// interface IChatMessage {
+//   message: string;
+// }
+
+const chatInputFormSchema = z.object({
+  message: z.string().min(3, {
+    message: "Message must be at least 3 characters",
+  }),
+});
+
+const ChatInputForm = ({
+  handleSendMessage,
+  isLoading,
+  error,
+  errorMessage,
+}: {
+  handleSendMessage: (message: string) => void;
+  isLoading: boolean;
+  error: boolean;
+  errorMessage: string;
+}) => {
+  // define form
+  const form = useForm<z.infer<typeof chatInputFormSchema>>({
+    resolver: zodResolver(chatInputFormSchema),
+    defaultValues: {
+      message: "",
+    },
+  });
+
+  // login handler
+  const loginHandler = (values: z.infer<typeof chatInputFormSchema>) => {
+    handleSendMessage(values.message);
+    form.reset();
+  };
+
+  return (
+    <Form {...form}>
+      {error && <p className="text-sm text-red-500">{errorMessage}</p>}
+      <form
+        onSubmit={form.handleSubmit(loginHandler)}
+        className="flex w-full items-center justify-between gap-x-2 p-4 "
+      >
+        <FormField
+          control={form.control}
+          name="message"
+          render={({ field }) => (
+            <FormItem className="w-full flex flex-col h-full relative">
+              <FormControl>
+                <Input placeholder="Start typing a message..." {...field} />
+              </FormControl>
+              <FormMessage className="absolute -top-6" />
+            </FormItem>
+          )}
+        />
+
+        <Button
+          type="submit"
+          className="flex items-center justify-center w-[150px]"
+        >
+          Send{" "}
+          {isLoading ? (
+            <Loader className="w-4 h-4 animate-spin ml-4" />
+          ) : (
+            <SendHorizonal className="w-4 h-4 ml-4" />
+          )}
+        </Button>
+      </form>
+    </Form>
+  );
 };
 
 export default ChatInputForm;

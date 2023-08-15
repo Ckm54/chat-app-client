0eda3af27b4f035f0e616a9a5b4ed2adf9d336ac
Ckm54 <mutaic84@gmail.com>
2023-08-15 19:49:36 +0300

    :technologist: update typings

--- src/types/typings.d.ts
+++ src/types/typings.d.ts
@@ -1,3 +1,6 @@
+import React from "react";
+import { Socket } from "socket.io-client";
+
 export interface IAuthUser extends IUser {
   __v: number;
   exp: number;
@@ -15,4 +18,33 @@ export interface IChatMessage {
   _id: string;
   fromSelf: boolean;
   message: string;
+}
+
+export interface ISocketContextState {
+  socket: Socket | undefined;
+  uid: string;
+  users: string[];
+  messages: IChatMessage[];
+}
+
+// socket context types
+export type TSocketContextActions =
+  | "update_uid"
+  | "update_users"
+  | "remove_user"
+  | "send-message"
+  | "add-user"
+  | "message-received"
+  | "update_socket";
+
+export type TSocketContextPayload = string | string[] | Socket | IChatMessage[];
+
+export interface ISocketContextActions {
+  type: TSocketContextActions;
+  payload: TSocketContextPayload;
+}
+
+export interface ISocketContextProps {
+  socketState: ISocketContextState;
+  socketDispatch: React.Dispatch<ISocketContextActions>;
 }
